72c66d302fb7704637ac722614371fadf544fa81
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-11-05 19:10:32 +0100

    postman collection

--- professionalTraining/src/main/java/com/professionalTraining/professionalTraining/repositories/ClassesRepository.java
+++ professionalTraining/src/main/java/com/professionalTraining/professionalTraining/repositories/ClassesRepository.java
@@ -4,6 +4,9 @@ import com.professionalTraining.professionalTraining.entities.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClassesRepository extends JpaRepository<Classes, Long> {
+    Optional<Classes> findById(Long id);
 }
--- professionalTraining/src/main/java/com/professionalTraining/professionalTraining/repositories/TrainingRepository.java
+++ professionalTraining/src/main/java/com/professionalTraining/professionalTraining/repositories/TrainingRepository.java
@@ -3,6 +3,8 @@ package com.professionalTraining.professionalTraining.repositories;
 import com.professionalTraining.professionalTraining.entities.Training;
 import com.professionalTraining.professionalTraining.entities.enums.TrainingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Repository
 public interface TrainingRepository extends JpaRepository<Training, Long> {
     List<Training> findByTitle(String title);
-    List<Training> findByStatus(TrainingStatus status);
+    @Query("SELECT t FROM Training t WHERE t.status = :status")
+    List<Training> findByStatus(@Param("status") TrainingStatus status);
     List<Training> findByTitleAndLevel(String title, String level);
 }
